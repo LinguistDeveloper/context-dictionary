06ce12d8d302b57730bf83e55235d4f0fd98d562
LinguistDeveloper <cook_richard@hotmail.com>
2016-11-11 16:45:15 +0100

    Add files via upload

---
+++ POS_tag_table.bat
@@ -0,0 +1,14 @@
+
+
+@ECHO OFF
+
+cd ../../..
+cd c:\TreeTagger
+
+set PATH=C:\TreeTagger\bin;%PATH%
+
+tag-english C:\wamp\www\rem_dupl_Across5\newfile.txt > temp.txt
+
+
+"C:\Windows\System32\cmd.exe" 
+
---
+++ run_optimize_tagged_data.bat
@@ -0,0 +1,15 @@
+
+
+@ECHO OFF
+
+cd ../../..
+
+cd python27
+rem_unhighlighted.py
+
+
+"C:\Windows\System32\cmd.exe" 
+
+
+#@start "" 192.168.1.117/em_dupl_Across5/searchterm.php
+"C:\Windows\System32\cmd.exe" 
